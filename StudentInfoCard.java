829f91485314c3f9d6c4d6704a8c24898633b87e
gracer34 <121206020+gracer34@users.noreply.github.com>
2022-12-21 19:11:34 -0600

    Update StudentInfoCard.java

--- StudentInfoCard.java
+++ StudentInfoCard.java
@@ -18,20 +18,20 @@ public class StudentInfoCard
             System.out.println("            ***********************************************************************************");
         
             System.out.print("My name is Grace Roland. ");
-            System.out.print("I was born on April 12, 2005, making me 17 years old. ");
+            System.out.print("I was born on (birthday), making me (age) years old. ");
             System.out.println("I attend Timothy Christian High School in Illinois. ");
             System.out.println("I am a senior this year and I live near Chicago.");
-            System.out.println("My phone number is 630-408-4248 and my school email is rolandgr@tcs.timothychristian.com.");
+            System.out.println("My phone number is (phone number) and my school email is (school email)");
             System.out.println("I am usually available through email anytime and by phone in the evenings and weekends");
             System.out.println("as I am not allowed to use my phone during school hours.");
-            System.out.println("My mom's phone number is 630-337-9897 and her email is jaimiejoy@gmail.com.");
+            System.out.println("My mom's phone number is (her number) and her email is (her email)");
             System.out.print("I am currently taking AP Calculus AB! ");
             System.out.println("I am super excited to learn about calculus as it has always fascinated me.");
             System.out.println("I have no previous computer programming experience but I am eager to learn as it looks super cool!");
             System.out.println("Since it's my senior year, I have a lot more control over the classes I am taking!");
             System.out.println("So, I took a ton of classes that interest me in the realms of math and science.");
             System.out.println("I thought I would give computer science a go, and maybe it would help me figure out what I should major in!");
-            System.out.print("(I also want that sweet AP credit)");
+            System.out.print("(I also want that AP credit)");
         }//end of main method
     }//end of class
     
